18a555566b1a61c89b13d86fb23f89301caf94d1
Jonathan Junio <jonathan.jm.nunes@gmail.com>
2020-10-07 13:46:14 -0300

    geração de graficos e eros

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data = pd.read_csv('data.csv')
+data = pd.read_csv('input/resistor-simulacao.csv')
 img_nome = 'grafico.png'
 colunas = data.columns.tolist()
-xlabel = colunas[1]
-ylabel = colunas[0]
-xerr = colunas[2]
-yerr = colunas[3]
+xlabel = colunas[0]
+ylabel = colunas[1]
+
 x_data = data[xlabel].to_numpy()
 y_data = data[ylabel].to_numpy()
 
-fig, ax = plt.subplots(figsize=(6.4, 4.9))
+fig, ax = plt.subplots(figsize=(4.8,4))
 ax.set_xlabel(xlabel)
 ax.set_ylabel(ylabel)
-ax.plot(x_data, y_data, marker='o', ls='', ms=5)
-plt.text(10, -10, 'exemplo')
+
+ax.plot(x_data, y_data, marker='o', ls='', ms=3)
+plt.yscale('linear')
+plt.xscale('linear')
+plt.title('Gráfico de corrente vs tensão (resistor)')
 
 if len(colunas) == 4:
+    xerr = colunas[2]
+    yerr = colunas[3]
     xerr = data[xerr].to_numpy()
     yerr = data[yerr].to_numpy()
-    ax.errorbar(x_data, y_data, yerr, xerr)
-plt.show()
+    ax.errorbar(x_data, y_data, yerr, xerr, linestyle='None', marker='')
+# plt.show()
+plt.savefig('output/'+img_nome)
